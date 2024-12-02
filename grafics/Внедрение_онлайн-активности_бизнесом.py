ed82c4dde62b9ee1c0bb7d8572b714e1c885f805
Даня <dany.ssimon2007@yandex.ru>
2024-12-02 19:29:13 +0300

    Create README.md

--- grafics/Внедрение_онлайн-активности_бизнесом.py
+++ grafics/Внедрение_онлайн-активности_бизнесом.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-
 data = pd.read_csv('Russia-COVID follow up 2020-full data.csv')
 
 online_activity = data['COVc4a'].value_counts()
---
+++ grafics/пока_в_процессе.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+data = pd.read_csv('Russia-COVID follow up 2020-full data.csv')
